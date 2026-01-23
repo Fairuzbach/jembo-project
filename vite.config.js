838441530f126bc86e0821d0ff0e63613bf0e90d
Fairuzbach <fairuzbachri10@gmail.com>
2026-01-23 16:20:41 +0700

    added new plugins

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     // },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/general-affair.css', 'resources/css/dashboard.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/general-affair.css', 'resources/css/dashboard.css', 'resources/js/facilities/dashboard.js'],
             refresh: true,
         }),
     ],
