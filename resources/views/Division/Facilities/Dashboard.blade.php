acf484049c63e2b00b559194bfe2f9ba23e36c05
Fairuzbach <fairuzbachri10@gmail.com>
2026-02-02 13:39:59 +0700

    add: resources css

--- resources/views/Division/Facilities/Dashboard.blade.php
+++ resources/views/Division/Facilities/Dashboard.blade.php
@@ -165,7 +165,7 @@
     <link rel="stylesheet" href="{{ asset('vendorDHTMLX/dhtmlxgantt.css') }}">
     <script src="{{ asset('vendorDHTMLX/dhtmlxgantt.js') }}"></script>
 
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 
     <div class="py-12 bg-[#F8FAFC]">
         <div id="dashboard-content" class="max-w-8xl mx-auto sm:px-6 lg:px-8 p-4 bg-[#F8FAFC]">
