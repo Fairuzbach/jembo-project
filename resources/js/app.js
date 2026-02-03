03687c6a036d2009fd4235b1291c5a08078aabbc
Fairuzbach <fairuzbachri10@gmail.com>
2026-02-03 10:47:47 +0700

    removing dashboard.js(facilities)

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import './bootstrap.js';
 import './general-affair.js'
 import './dashboard.js'
 import './gantt-init.js'
-import './facilities/dashboard.js'
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
 
