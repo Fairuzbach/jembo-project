454fdb7dcdf0f6e279543d7907bd3b96137eebea
Fairuzbach <fairuzbachri10@gmail.com>
2026-02-03 10:47:07 +0700

    removing resource/js/facilities/dashboard/js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     // },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/general-affair.css', 'resources/css/dashboard.css', 'resources/js/facilities/dashboard.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/general-affair.css', 'resources/css/dashboard.css'],
             refresh: true,
         }),
     ],
