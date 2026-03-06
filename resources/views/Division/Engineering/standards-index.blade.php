47d7dce677f6e9b1fa891fe4cca26658603971c2
Fairuzbach <fairuzbachri10@gmail.com>
2026-03-06 13:30:20 +0700

    remove pengedcekan 2-4

--- resources/views/Division/Engineering/standards-index.blade.php
+++ resources/views/Division/Engineering/standards-index.blade.php
@@ -37,7 +37,9 @@
                 @php
                     // Kelompokkan data agar rapi saat ditampilkan
                     $plantAStandards = $standards->where('plant', 'Plant A');
-                    $autowireStandards = $standards->where('plant', 'Autowire');
+
+                    // Filter khusus Autowire agar hanya memanggil Mesin Multi Drawing 3 Honta (ID: 55)
+                    $autowireStandards = $standards->where('plant', 'Autowire')->where('machine_id', 55);
                 @endphp
 
                 <div class="p-0 sm:p-6 bg-slate-50">
