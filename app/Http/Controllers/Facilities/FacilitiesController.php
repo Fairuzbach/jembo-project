52908b68fa2dc6df4475e2482ea70b452621fd24
Fairuzbach <fairuzbachri10@gmail.com>
2026-04-22 09:21:11 +0700

    adding excluded plant for facility digital work order

--- app/Http/Controllers/Facilities/FacilitiesController.php
+++ app/Http/Controllers/Facilities/FacilitiesController.php
@@ -134,7 +134,7 @@ class FacilitiesController extends Controller
         $pageIds    = $workOrders->pluck('id')->toArray();
 
         // DATA PENDUKUNG
-        $excludedPlants = ['pe', 'QC FO', 'HC', 'Plant F', 'FA', 'IT', 'Sales', 'Marketing', 'RM Office', 'RM 1', 'RM 2', 'RM 3', 'RM 5', 'MT', 'FH', 'FO', 'QR', 'Plant Tools', 'Gudang Jadi', 'QC Lab', 'Konstruksi', 'GA - JAKARTA', 'QC LV', 'QC MV', 'JEMBO ENERGINDO', 'ACCOUNTING'];
+        $excludedPlants = ['pe', 'QC FO', 'HC', 'Plant F', 'FA', 'IT', 'Sales', 'Marketing', 'RM Office', 'RM 1', 'RM 2', 'RM 3', 'RM 5', 'MT', 'FH', 'FO', 'QR', 'Plant Tools', 'Gudang Jadi', 'QC Lab', 'Konstruksi', 'GA - JAKARTA', 'GA - TANGERANG', 'QC LV', 'QC MV', 'JEMBO ENERGINDO', 'ACCOUNTING', 'Gudang Scrap', 'Workshop Electric', 'SS', 'PP'];
         $listPlants     = \App\Models\Engineering\Plant::whereNotIn('name', $excludedPlants)->get();
         $machines       = \App\Models\Engineering\Machine::all();
         $technicians    = \App\Models\FacilityTech::all();
