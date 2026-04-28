ffce54a526e7305520c6ba1fe2fb4e953f1e03e7
Fairuzbach <fairuzbachri10@gmail.com>
2026-04-28 16:33:45 +0700

    nonaktif pdf

--- resources/views/components/facilityIndex/modal-detail.blade.php
+++ resources/views/components/facilityIndex/modal-detail.blade.php
@@ -225,7 +225,7 @@
 
                         <div class="mt-8 pt-4 border-t border-slate-100 flex justify-end items-center gap-3">
                             {{-- TOMBOL EXPORT PDF --}}
-                            <a :href="'/fh/' + ticket.id + '/pdf'" target="_blank"
+                            {{-- <a :href="'/fh/' + ticket.id + '/pdf'" target="_blank"
                                 class="px-5 py-2.5 bg-red-50 hover:bg-red-100 text-red-600 rounded-xl font-bold transition-colors flex items-center gap-2 border border-red-200">
                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -233,7 +233,7 @@
                                     </path>
                                 </svg>
                                 Export PDF
-                            </a>
+                            </a> --}}
 
                             {{-- TOMBOL TUTUP --}}
                             <button @click="showDetailModal = false"
